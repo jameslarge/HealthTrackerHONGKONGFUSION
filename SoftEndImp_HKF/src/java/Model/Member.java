f52f211b346554be88b823663bc9840cb15326e5
unknown <xfu13dcu@CMPLAB1-02.cmp.uea.ac.uk>
2015-03-04 12:57:25 +0000

    Little bit of Member added and added find method in User

---
+++ SoftEndImp_HKF/src/java/Model/Member.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Member extends User{
+    
+    String forename;
+    String surname;
+    //Goal goals;
+    //Health health;
+
+    public Member(String username, String password, String email) {
+        super(username, password, email);
+    }
+    
+    
+    
+}
