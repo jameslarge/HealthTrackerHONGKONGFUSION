8e9dd197ab23ed23af919b2169dea84de82ac8c1
unknown <vtv13qau@CMPLAB1-03.cmp.uea.ac.uk>
2015-03-04 14:12:25 +0000

    added a temp db.sql

---
+++ db.sql
@@ -0,0 +1,17 @@
+DROP TABLE userDetails CASCADE;
+
+CREATE TABLE userDetails
+(
+	id SERIAL,
+	email VARCHAR(100),
+	password VARCHAR(20),
+	username VARCHAR (100),
+	PRIMARY KEY (id)
+	
+);
+
+---------------------------------------------
+----POPULATING DATABASE FOR TESTING PURPOSES
+
+INSERT INTO student(email, password, username)
+VALUES ('pedo', 'pedo', 'PEDO');
