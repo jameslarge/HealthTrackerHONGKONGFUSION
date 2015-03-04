fb3439f24737ff1efd9c37a66046d3f023b3bdb9
unknown <xfu13dcu@CMPLAB1-02.cmp.uea.ac.uk>
2015-03-04 12:32:32 +0000

    Added Users stuff, getter and setter methods

--- SoftEndImp_HKF/src/java/Model/User.java
+++ SoftEndImp_HKF/src/java/Model/User.java
@@ -1,12 +1,46 @@
 /**
- * This Class will Model the Users shi
+ * This Class will Model the Users information
  */
 package Model;
 
 /**
- *
+ * 
  * @author xfu13dcu
  */
 public class User {
     
+   private String username;
+   private String password;
+   private String email;
+   
+   User(String username, String password, String email){
+       this.username = username;
+       this.password = password;
+       this.email = email;
+   }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   
 }
