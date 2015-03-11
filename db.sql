dc5917039ce46618898ab6261b7bc1eec138a115
unknown <vtv13qau@CMPLAB4-07.UEA.AC.UK>
2015-03-11 15:40:03 +0000

    userDetails -> member
    Added more tables
    Updated database
    Created ER diagram which is linked on Trello (google drive)
    
    TO BE CHANGED:
    
    any reference to userDetails

--- db.sql
+++ db.sql
@@ -1,17 +1,48 @@
-﻿DROP TABLE userDetails CASCADE;
+﻿DROP TABLE member CASCADE;
+DROP TABLE physicalHealth CASCADE;
+DROP TABLE weightProgress CASCADE;
 
-CREATE TABLE userDetails
+CREATE TABLE member
 (
-	id SERIAL,
-	email VARCHAR(100),
-	password VARCHAR(20),
-	username VARCHAR (100),
+	id 				SERIAL,
+	email 			VARCHAR(100),
+	password		VARCHAR(20),
+	username 		VARCHAR (100),
+	name			VARCHAR (100),
+	surname			VARCHAR (100).
+	
+	
+	
 	PRIMARY KEY (id)
 	
 );
 
+
+
+CREATE TABLE physicalHealth
+(
+	id 				SERIAL,
+	height 			INT,
+	memberID		INT REFERENCES member(id),
+	PRIMARY KEY (id)
+
+
+);
+
+CREATE TABLE weightProgress
+(
+	id				 SERIAL,
+	physicalHealthID INT	REFERENCES physicalHealth(id),
+	weightDate 		 DATE,
+	weight			 INT,
+	
+	PRIMARY KEY (id)
+);
+
+
+
 ---------------------------------------------
 ----POPULATING DATABASE FOR TESTING PURPOSES
 
-INSERT INTO userDetails(email, password, username)
+INSERT INTO member(email, password, username)
 	VALUES ('pedo', 'pedo', 'PEDO');
